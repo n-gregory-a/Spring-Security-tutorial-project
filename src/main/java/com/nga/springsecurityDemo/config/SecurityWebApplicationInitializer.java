06d2c9c7e69d2cfd70efd27a00546372f257f5b3
Grigory <ovonely@gmail.com>
2020-01-05 17:52:37 +0700

    Spring Security - basic security:   - creating DemoSecurityConfig class and defining users   - creating SecurityWebApplicationInitializer class

---
+++ src/main/java/com/nga/springsecurityDemo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.nga.springsecurityDemo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
