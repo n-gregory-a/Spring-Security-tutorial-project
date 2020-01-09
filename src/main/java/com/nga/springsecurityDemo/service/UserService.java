32a85323558010220235d011758f756721e66d74
Grigory <ovonely@gmail.com>
2020-01-09 19:27:08 +0700

    Spring Security - user registration:   - creating UserService and UserServiceImpl classes

--- src/main/java/com/nga/springsecurityDemo/service/UserService.java
+++ src/main/java/com/nga/springsecurityDemo/service/UserService.java
@@ -1,7 +1,7 @@
 package com.nga.springsecurityDemo.service;
 
+import com.nga.springsecurityDemo.entity.User;
 import com.nga.springsecurityDemo.user.CrmUser;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UserService extends UserDetailsService {
