59e629f05a955f227803ad05872b22b15210ae2a
Grigory <ovonely@gmail.com>
2020-01-09 19:19:55 +0700

    Spring Security - user registration:   - defining a BCryptPasswordEncoder and DaoAuthenticationProvider beans

--- src/main/java/com/nga/springsecurityDemo/config/DemoSecurityConfig.java
+++ src/main/java/com/nga/springsecurityDemo/config/DemoSecurityConfig.java
@@ -1,12 +1,15 @@
 package com.nga.springsecurityDemo.config;
 
+import com.nga.springsecurityDemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -14,15 +17,14 @@ import javax.sql.DataSource;
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    // add a reference to security data source
+    // add a reference to our security data source
     @Autowired
-    private DataSource securityDataSource;
+    private UserService userService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        // use jdbc authentication
-        auth.jdbcAuthentication().dataSource(securityDataSource);
+        auth.authenticationProvider(authenticationProvider());
 
     }
 
@@ -42,5 +44,20 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .exceptionHandling().accessDeniedPage("/access-denied");
+    }
+
+    // bcrypt bean definition
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // authenticationProvider bean definition
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
     }
 }
