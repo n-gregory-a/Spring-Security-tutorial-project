3f54ec204a6ad21bbdb928978b3b24a43b272fae
Grigory <ovonely@gmail.com>
2020-01-10 15:06:46 +0700

    Spring Security - user registration:   - some fixes and... IT'S WORKING NOW!!! FINALLY!!!

--- src/main/java/com/nga/springsecurityDemo/entity/User.java
+++ src/main/java/com/nga/springsecurityDemo/entity/User.java
@@ -28,7 +28,7 @@ public class User {
     private String email;
 
     @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinTable(name = "user_roles",
+    @JoinTable(name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Collection<Role> roles;
